e56d6dfebbaef36603cf5b3d64c71639865bd2e5
Lewis Wigmore <lewis.wigmore77@gmail.com>
2023-10-10 23:23:04 +0100

    Added optional aguments for decoding methods

--- Encoded_String_Decoder/decoder.py
+++ Encoded_String_Decoder/decoder.py
@@ -44,12 +44,13 @@ def is_readable_text(s):
     ratio = printable_chars / total_chars
     return ratio > threshold
 
-def detect_and_decode(file_path):
+def detect_and_decode(file_path, encoding=None):
     """
     Detect and decode encoded strings within a given file.
-    
+
     Args:
         file_path (str): Path to the file to be analyzed.
+        encoding (str): Encoding method to be used.
 
     Returns:
         list: A list of tuples containing original encoded string, encoding type, and decoded string.
@@ -61,43 +62,44 @@ def detect_and_decode(file_path):
     pattern_hex = rb'[0-9a-fA-F]+'
     pattern_url = rb'(?:%[0-9a-fA-F]{2}|[A-Za-z0-9_.~!$&\'()*+,;=:@/-])+'
 
-        
     encoded_strings_b64 = re.findall(pattern_b64, content)
     encoded_strings_hex = re.findall(pattern_hex, content)
     encoded_strings_url = re.findall(pattern_url, content)
         
     decoded_data = []
 
-    for encoded in encoded_strings_b64:
-        encoded_str = encoded.decode('utf-8', 'ignore')
-        if is_valid_base64(encoded_str):
-            try:
-                decoded = base64.b64decode(encoded).decode('utf-8')
-                if decoded.strip() and is_readable_text(decoded):  
-                    decoded_data.append((encoded_str, "Base64", decoded))
-            except:
-                pass 
+    if encoding in [None, "Base64"]:
+        for encoded in encoded_strings_b64:
+            encoded_str = encoded.decode('utf-8', 'ignore')
+            if is_valid_base64(encoded_str):
+                try:
+                    decoded = base64.b64decode(encoded).decode('utf-8')
+                    if decoded.strip() and is_readable_text(decoded):  
+                        decoded_data.append((encoded_str, "Base64", decoded))
+                except:
+                    pass 
 
     MIN_HEX_LENGTH = 4  # minimal length to reduce noise
 
-    for encoded in encoded_strings_hex:
-        encoded_str = encoded.decode('utf-8', 'ignore')
-        if is_valid_hex(encoded_str) and len(encoded_str) >= MIN_HEX_LENGTH and encoded not in encoded_strings_b64:
-            decoded = decode_hex(encoded_str)
-            if decoded and decoded.strip() and is_readable_text(decoded):
-                decoded_data.append((encoded_str, "Hexadecimal", decoded))
+    if encoding in [None, "Hexadecimal"]:
+        for encoded in encoded_strings_hex:
+            encoded_str = encoded.decode('utf-8', 'ignore')
+            if is_valid_hex(encoded_str) and len(encoded_str) >= MIN_HEX_LENGTH and encoded not in encoded_strings_b64:
+                decoded = decode_hex(encoded_str)
+                if decoded and decoded.strip() and is_readable_text(decoded):
+                    decoded_data.append((encoded_str, "Hexadecimal", decoded))
 
-
-    for encoded in encoded_strings_url:
-        encoded_str = encoded.decode('utf-8', 'ignore')
-        try:
-            # Additional checks to be more confident that it is really URL encoded data
-            if "%" in encoded_str and encoded_str not in [e[0] for e in decoded_data]:
-                decoded = urllib.parse.unquote(encoded_str)
-                if decoded.strip() and is_readable_text(decoded):
-                    decoded_data.append((encoded_str, "URL Encoding", decoded))
-        except:
-            pass
+    if encoding in [None, "URL Encoding"]:
+        for encoded in encoded_strings_url:
+            encoded_str = encoded.decode('utf-8', 'ignore')
+            try:
+                # Additional checks to be more confident that it is really URL encoded data
+                if "%" in encoded_str and encoded_str not in [e[0] for e in decoded_data]:
+                    decoded = urllib.parse.unquote(encoded_str)
+                    if decoded.strip() and is_readable_text(decoded):
+                        decoded_data.append((encoded_str, "URL Encoding", decoded))
+            except:
+                pass
     
     return decoded_data
 
@@ -106,7 +108,9 @@ def main():
     parser = argparse.ArgumentParser(description="Decode encoded strings in a file.")
     parser.add_argument("input", help="Path to the input file.")
     parser.add_argument("output", help="Path to the output file.")
-    
+    parser.add_argument("-e", "--encoding", choices=["Base64", "Hexadecimal", "URL Encoding"],
+                        help="Specify the encoding method to detect (default: all).")
+
     args = parser.parse_args()
     
     # Ensure the output file has a valid extension and get output format
@@ -114,8 +118,8 @@ def main():
     if not ext or ext.lower() not in ['.csv', '.json']:
         sys.exit("Output file must have a valid extension (.csv or .json).")
     
-    # Get decoded data
-    decoded_data = detect_and_decode(args.input)
+    # Pass the selected encoding method to detect_and_decode()
+    decoded_data = detect_and_decode(args.input, args.encoding)
     
     # Output to CSV
     if ext.lower() == '.csv':
