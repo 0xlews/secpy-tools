9a79709f0898fc4b50dcd8d6436c9f91318fc163
Lewis Wigmore <lewis.wigmore77@gmail.com>
2023-10-10 22:43:03 +0100

    Implemented encoded URL detection functionality

--- Encoded_String_Decoder/decoder.py
+++ Encoded_String_Decoder/decoder.py
@@ -3,6 +3,7 @@ import base64
 import re
 import binascii  
 import sys
+import urllib.parse
 
 # Ensure script is run with Python 3.10.4 or higher
 if sys.version_info < (3, 10, 4):
@@ -10,13 +11,20 @@ if sys.version_info < (3, 10, 4):
 
 def is_valid_base64(s):
     """Check if a string is valid Base64 encoding."""
-    pattern = re.compile(r'^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$')
+    pattern = re.compile(r'^(?:%[0-9a-fA-F]{2}|[A-Za-z0-9_.~!$&\'()*+,;=:@/-])+$')
     return bool(pattern.match(s))
 
 def is_valid_hex(s):
     """Check if a string is valid Hexadecimal encoding."""
     pattern = re.compile(r'^[0-9a-fA-F]+$')
     return bool(pattern.match(s))
+
+def is_valid_url(s):
+    """Check if a string may be URL-encoded."""
+    # Pattern looks for '%' followed by two hexadecimal digits.
+    pattern = re.compile(r'(?:%[0-9a-fA-F]{2}|[A-Za-z0-9_.~!$&\'()*+,;=:@/-])+')
+    return bool(pattern.match(s))
+
 
 def decode_hex(s):
     """Try to decode a hexadecimal string into utf-8, return None if unsuccessful."""
@@ -48,9 +56,12 @@ def detect_and_decode(file_path):
         
     pattern_b64 = rb'(?:[A-Za-z0-9+/]{4}){2,}(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?'
     pattern_hex = rb'[0-9a-fA-F]+'
+    pattern_url = rb'(?:%[0-9a-fA-F]{2}|[A-Za-z0-9_.~!$&\'()*+,;=:@/-])+'
+
         
     encoded_strings_b64 = re.findall(pattern_b64, content)
     encoded_strings_hex = re.findall(pattern_hex, content)
+    encoded_strings_url = re.findall(pattern_url, content)
         
     decoded_data = []
 
@@ -64,14 +75,29 @@ def detect_and_decode(file_path):
             except:
                 pass 
 
+    MIN_HEX_LENGTH = 4  # minimal length to reduce noise
+
     for encoded in encoded_strings_hex:
         encoded_str = encoded.decode('utf-8', 'ignore')
-        if is_valid_hex(encoded_str) and encoded not in encoded_strings_b64:
+        if is_valid_hex(encoded_str) and len(encoded_str) >= MIN_HEX_LENGTH and encoded not in encoded_strings_b64:
             decoded = decode_hex(encoded_str)
             if decoded and decoded.strip() and is_readable_text(decoded):
                 decoded_data.append((encoded_str, "Hexadecimal", decoded))
+
+
+    for encoded in encoded_strings_url:
+        encoded_str = encoded.decode('utf-8', 'ignore')
+        try:
+            # Additional checks to be more confident that it is really URL encoded data
+            if "%" in encoded_str and encoded_str not in [e[0] for e in decoded_data]:
+                decoded = urllib.parse.unquote(encoded_str)
+                if decoded.strip() and is_readable_text(decoded):
+                    decoded_data.append((encoded_str, "URL Encoding", decoded))
+        except:
+            pass
     
     return decoded_data
+
 
 def main():
     """Main function to parse arguments and handle file I/O."""
@@ -86,7 +112,8 @@ def main():
     # Write the original string, its encoding method, and the decoded string to the output file
     with open(args.output, 'w', encoding='utf-8') as f:
         for original, encoding, data in decoded_data:
-            f.write(f"{original}, {encoding}, {data}\n")
+            f.write(f'"{original}", "{encoding}", "{data}"\n')
+
 
 if __name__ == "__main__":
     main()
