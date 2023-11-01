b2884e77ef2ccecc72c5c5778dcf505547906131
Lewis Wigmore <lewis.wigmore77@gmail.com>
2023-11-01 16:59:41 +0000

    Code clean up

--- CaesarCipher_Tool/CaesarCipher-Tool.py
+++ CaesarCipher_Tool/CaesarCipher-Tool.py
@@ -1,17 +1,18 @@
 import argparse
 
+# Function to perform Caesar Cipher decryption
 def caesar_cipher(text, shift):
     decrypted_text = ""
     for char in text:
-        if char.isalpha():
-            is_upper = char.isupper()
-            char = char.lower()
-            shifted = chr(((ord(char) - ord('a') - shift) % 26) + ord('a'))
-            if is_upper:
+        if char.isalpha():  
+            is_upper = char.isupper() 
+            char = char.lower()  
+            shifted = chr(((ord(char) - ord('a') - shift) % 26) + ord('a'))  # Decryption logic
+            if is_upper:  # Convert back to uppercase if the original character was uppercase
                 shifted = shifted.upper()
             decrypted_text += shifted
         else:
-            decrypted_text += char
+            decrypted_text += char  # Preserve non-alphabetic characters
     return decrypted_text
 
 def main():
@@ -21,10 +22,9 @@ def main():
     args = parser.parse_args()
     encrypted_text = args.text
 
-    for shift in range(26):
+    for shift in range(26):  # Try all possible shift values (0 to 25)
         decrypted_text = caesar_cipher(encrypted_text, shift)
         print(f"Shift {shift}: {decrypted_text}")
 
 if __name__ == "__main__":
     main()
-
