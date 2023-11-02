b5b8f1f0f781ceb202fa2a46cc20a293ec82a25f
Lewis Wigmore <lewis.wigmore77@gmail.com>
2023-11-02 11:36:06 +0000

    Added io validation

--- CSV_Parser/clean.py
+++ CSV_Parser/clean.py
@@ -2,30 +2,41 @@ import pandas as pd
 import argparse
 
 def clean_csv(input_file, output_file, remove_duplicates=True):
-    # Read the input CSV file
-    df = pd.read_csv(input_file)
+    try:
+        # Check if the input file has a .csv extension
+        if not input_file.endswith('.csv'):
+            raise ValueError("Error: The input file must have a .csv extension.")
 
-    # Fill missing values with an empty string 
-    df = df.fillna('')
+        df = pd.read_csv(input_file)
 
-    if remove_duplicates:
-        # Remove duplicate rows
-        df = df.drop_duplicates()
+        # Fill missing values with an empty string
+        df = df.fillna('')
 
-        # Remove duplicate columns
-        df = df.loc[:, ~df.columns.duplicated()]
+        if remove_duplicates:
+            # Remove duplicate rows
+            df = df.drop_duplicates()
 
-    # Save the cleaned data to a new CSV file
-    df.to_csv(output_file, index=False)
-    print(f"Cleaned data saved to {output_file}")
+            # Remove duplicate columns
+            df = df.loc[:, ~df.columns.duplicated()]#
+
+        # Ensure the output file has a .csv extension
+        if not output_file.endswith('.csv'):
+            output_file += '.csv'
+
+        df.to_csv(output_file, index=False)
+        print(f"Cleaned data saved to {output_file}")
+
+    except pd.errors.ParserError:
+        print(f"Error: {input_file} is not a valid CSV file. Please provide a valid CSV file.")
+    except ValueError as e:
+        print(str(e))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Clean a CSV file by filling missing values with empty strings.")
     parser.add_argument('input_file', type=str, help="Path to the input CSV file")
-    parser.add_argument('output_file', type=str, help="Path to the output cleaned CSV file")
+    parser.add_argument('output_file', type=str, help="Path to the output cleaned CSV file. Will append .csv if not provided.")
     parser.add_argument('--remove_duplicates', action='store_true', help="Remove duplicate rows and columns")
 
     args = parser.parse_args()
 
     clean_csv(args.input_file, args.output_file, args.remove_duplicates)
-
